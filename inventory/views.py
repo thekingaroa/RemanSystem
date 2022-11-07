d712facb719d9c6580d22232e177598d1d96655a
hmnvkv <manrique.hamed1610@gmail.com>
2022-11-06 20:29:31 -0600

    Validaciones corregidas

--- inventory/views.py
+++ inventory/views.py
@@ -5,22 +5,32 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'home.html')
 
+
 def SignUp(request):
-    
+
     if request.method == 'GET':
         return render(request, 'signUp.html', {
             'form': UserCreationForm
         })
-    else: 
+    else:
         if request.POST['password1'] == request.POST['password2']:
-           try:
+            try:
                 # Register User
-                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
                 user.save()
                 return HttpResponse('Usuario creado correctamente')
-           except:
-               return HttpResponse('el usuario ya existe')
-        return HttpResponse('Las contraseñas no coinciden')
+            except:
+                return render(request, 'signUp.html', {
+                    'form': UserCreationForm,
+                    'error': 'El usuario ya existe'
+                })
+        return render(request, 'signUp.html', {
+            'form': UserCreationForm,
+            'error': 'Contraseñas no coinciden'
+        })
